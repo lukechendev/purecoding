05d03fbe10cc683ebcb3362bc1b16254f378aa4c
Luke Chen <lchen154@ford.com>
2020-03-22 19:44:34 -0400

    T003_queue

--- workspace_standard/data-structure/src/lchen/datastructure/queue/Queue.java
+++ workspace_standard/data-structure/src/lchen/datastructure/queue/Queue.java
@@ -1,9 +1,62 @@
 package lchen.datastructure.queue;
 
-public class Queue {
+import java.util.NoSuchElementException;
 
-	// To test
-	public static void main(String[] args) {
-		System.out.println("Queue");
-	}
+public class Queue {
+    Node head; // always remove from head
+    Node tail; // always add to tail
+
+    public void add(int data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        tail = node;
+    }
+
+    public int poll() {
+        if (head != null) {
+            int data = head.data;
+            head = head.next;
+
+            if (head == null) {
+                tail = null;
+            }
+
+            return data;
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    public int peek() {
+        if (head != null) {
+            return head.data;
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    // To test
+    public static void main(String[] args) {
+        System.out.println("Queue");
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
 }
