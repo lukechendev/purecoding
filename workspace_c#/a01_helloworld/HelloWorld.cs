fe19322fd9c870d56b9ee677fce578f6c1c746b9
Luke Chen <lchen.dev@gmail.com>
2021-01-24 22:24:01 -0500

    Adding c#

---
+++ workspace_c#/a01_helloworld/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace a01_helloworld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World home HelloWorld!");
+        }
+    }
+
+    class HelloWorld2
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World home HelloWorld2!");
+
+            Console.WriteLine("Please tell me two truth and one lie: ");
+
+            var statements = new string[3];
+            for (int i = 0; i < statements.Length; ++i) {
+                statements[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("The lie is: {0}", GetLie(statements));
+        }
+        private static string GetLie(string[] statements)
+        {
+            var rand = new Random();
+            return statements[rand.Next(3)];
+        }
+    }
+}
