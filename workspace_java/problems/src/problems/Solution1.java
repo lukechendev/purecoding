4b9c695fdaabe1c6d54952387b5998f87512a0cb
Luke Chen <lchen.dev@gmail.com>
2021-02-14 22:54:03 -0500

    Reformat

--- workspace_java/problems/src/problems/Solution1.java
+++ workspace_java/problems/src/problems/Solution1.java
@@ -12,19 +12,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Solution1 {
 	// <k, v>
 	Map<Integer, Integer> m1 = new HashMap<>();
-	
+
 	// <v, k>
 	TreeMap<Integer, Set<Integer>> m2 = new TreeMap<>();
-	
+
 	public void increase(int k) {
 		int v = m1.getOrDefault(k, 0);
-		
+
 		Set<Integer> keys = m2.get(v);
 		if (keys != null) {
 			keys.remove(k);
@@ -34,20 +33,20 @@ public class Solution1 {
 				m2.put(v, keys);
 			}
 		}
-		
+
 		m1.put(k, ++v);
-		
+
 		Set<Integer> newKeys = m2.getOrDefault(v, new HashSet<Integer>());
 		newKeys.add(k);
 		m2.put(v, newKeys);
 	}
-	
+
 	public void decrease(int k) {
 		int v = m1.getOrDefault(k, 0);
 		if (v == 0) {
 			return;
 		}
-		
+
 		Set<Integer> keys = m2.get(v);
 		keys.remove(k);
 		if (keys.size() == 0) {
@@ -55,34 +54,34 @@ public class Solution1 {
 		} else {
 			m2.put(v, keys);
 		}
-		
+
 		if (--v == 0) {
 			m1.remove(k);
 			return;
 		}
-		
+
 		m1.put(k, v);
-		
-		Set<Integer> newKeys = m2.getOrDefault(v,  new HashSet<Integer>());
+
+		Set<Integer> newKeys = m2.getOrDefault(v, new HashSet<Integer>());
 		newKeys.add(k);
-		m2.put(v,  newKeys);
+		m2.put(v, newKeys);
 	}
-	
+
 	public int getKeyWithMaxValue() {
 		Map.Entry<Integer, Set<Integer>> last = m2.lastEntry();
 		if (last == null || last.getValue().size() == 0) {
 			return 0;
 		}
-		
+
 		return last.getValue().iterator().next();
 	}
-	
+
 	public int getKeyWithMinValue() {
 		Map.Entry<Integer, Set<Integer>> first = m2.firstEntry();
 		if (first == null || first.getValue().size() == 0) {
 			return 0;
 		}
-		
+
 		return first.getValue().iterator().next();
 	}
 }
--- workspace_java/problems/src/problems/Test1.java
+++ workspace_java/problems/src/problems/Test1.java
@@ -1,28 +1,28 @@
 package problems;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Test1 {
-	
+
 	private Solution1 s;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		s = new Solution1();
-		
+
 		s.increase(1);
-		
+
 		s.increase(2);
 		s.increase(2);
 		s.increase(2);
-		
+
 		s.increase(3);
 		s.increase(3);
-		
+
 		s.increase(4);
 		s.increase(4);
 		s.increase(4);
@@ -37,10 +37,10 @@ class Test1 {
 	void testIncrease() {
 		assertEquals(4, s.getKeyWithMaxValue());
 		assertEquals(1, s.getKeyWithMinValue());
-		
+
 		s.increase(2);
 		s.increase(2);
-		
+
 		assertEquals(2, s.getKeyWithMaxValue());
 		assertEquals(1, s.getKeyWithMinValue());
 	}
@@ -49,10 +49,10 @@ class Test1 {
 	void testDecrease() {
 		assertEquals(4, s.getKeyWithMaxValue());
 		assertEquals(1, s.getKeyWithMinValue());
-		
+
 		s.decrease(4);
 		s.decrease(4);
-		
+
 		assertEquals(2, s.getKeyWithMaxValue());
 		assertEquals(1, s.getKeyWithMinValue());
 	}
@@ -60,36 +60,36 @@ class Test1 {
 	@Test
 	void testGetKeyWithMaxValue() {
 		assertEquals(4, s.getKeyWithMaxValue());
-		
+
 		s.increase(2);
 		s.increase(2);
-		
+
 		assertEquals(2, s.getKeyWithMaxValue());
-		
+
 		s.increase(4);
 		s.decrease(2);
-		
+
 		assertEquals(4, s.getKeyWithMaxValue());
-		
+
 		s.decrease(4);
 		s.decrease(4);
-		
+
 		assertEquals(2, s.getKeyWithMaxValue());
 	}
 
 	@Test
 	void testGetKeyWithMinValue() {
 		assertEquals(1, s.getKeyWithMinValue());
-		
+
 		s.increase(1);
 		s.increase(1);
-		
+
 		assertEquals(3, s.getKeyWithMinValue());
-		
+
 		s.decrease(4);
 		s.decrease(4);
 		s.decrease(4);
-		
+
 		assertEquals(4, s.getKeyWithMinValue());
 	}
 
