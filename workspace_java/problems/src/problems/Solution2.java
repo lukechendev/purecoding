9915d74530786b1794a2eaaab159cd45487e7576
Luke Chen <lchen.dev@gmail.com>
2021-05-21 21:57:04 -0400

    add two java problems

---
+++ workspace_java/problems/src/problems/Solution2.java
@@ -0,0 +1,14 @@
+package problems;
+
+/**
+ * https://www.lalitkundu.com/#h.8z6kgjsj9y5v
+ * 
+ * Design a data structure which can support following operations
+ * Insert x.
+ * Remove x.
+ * Output a psuedo random value from the container.
+ */
+
+public class Solution2 {
+	
+}
---
+++ workspace_java/problems/src/problems/Solution3.java
@@ -0,0 +1,15 @@
+package problems;
+
+/**
+ * https://www.linkedin.com/posts/lalitkundu_codinginterviewweekly-probingwithlalitkundu-activity-6756094342559100928-b7PR/
+ * https://www.lalitkundu.com/#h.4bpgrpg4wnpt
+ * 
+ * https://www.lintcode.com/problem/79
+ * 
+ * Given two strings A and B, find the longest common contiguous substring between them.
+ *
+ */
+
+public class Solution3 {
+
+}
