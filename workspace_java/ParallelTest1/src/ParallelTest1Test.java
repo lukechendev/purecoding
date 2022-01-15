022f4c74ea1128cff26d127b0192135bb9bd6074
Luke Chen <lchen.dev@gmail.com>
2022-01-15 18:50:11 -0500

    cleanup

--- workspace_java/ParallelTest1/src/ParallelTest1Test.java
+++ workspace_java/ParallelTest1/src/ParallelTest1Test.java
@@ -1,11 +1,8 @@
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ParallelTest1Test {
 
