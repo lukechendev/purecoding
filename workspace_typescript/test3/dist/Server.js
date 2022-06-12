018b253e11dc46c9d7feb6a010f1dd2a452c53ed
Luke Chen <lchen.dev@gmail.com>
2022-06-12 14:22:54 -0400

    add server async

--- workspace_typescript/test3/dist/Server.js
+++ workspace_typescript/test3/dist/Server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class BaseServer {
     constructor(address, port) {
         console.log("construct BaseServer");
@@ -8,16 +17,52 @@ class BaseServer {
 }
 class DbServer extends BaseServer {
     constructor(address, port) {
-        console.log("construct DbServer");
         super(address, port);
+        this.isActive = false;
+        this.data = "";
+        this.error = "";
+        console.log("construct DbServer");
     }
     startServer() {
         console.log(`Started DB server at ${this.address}:${this.port}`);
+        this.isActive = true;
     }
     stopServer() {
         console.log("Stopped DB server");
+        this.isActive = false;
+    }
+    getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.syncDataFromServer();
+            console.log(`Data from db server: ${this.data}`);
+        });
+    }
+    syncDataFromServer() {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log("Syncing data from DB server");
+            try {
+                this.data = yield this.getDataAsync();
+            }
+            catch (e) {
+                this.error = String(e);
+                console.log(`Error from db server: ${this.error}`);
+            }
+        });
+    }
+    getDataAsync() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (this.isActive) {
+                    resolve("db at " + new Date().getTime());
+                }
+                else {
+                    reject("500 server error");
+                }
+            }, 1000);
+        });
     }
 }
 const dbServer = new DbServer("127.0.0.1", "80");
 dbServer.startServer();
+dbServer.getData();
 dbServer.stopServer();
--- workspace_typescript/test3/src/Server.ts
+++ workspace_typescript/test3/src/Server.ts
@@ -1,6 +1,7 @@
 interface IServer {
     startServer(): void;
     stopServer(): void;
+    getData(): any;
 }
 
 abstract class BaseServer implements IServer {
@@ -14,23 +15,59 @@ abstract class BaseServer implements IServer {
     }
     abstract startServer(): void;
     abstract stopServer(): void;
+    abstract getData(): any;
 }
 
 class DbServer extends BaseServer {
+    private isActive: boolean = false;
+    private data: string = "";
+    private error: string = "";
+
     constructor(address: string, port: string) {
-        console.log("construct DbServer")
         super(address, port);
+        console.log("construct DbServer")
     }
 
     startServer(): void {
         console.log(`Started DB server at ${this.address}:${this.port}`);
+        this.isActive = true;
     }
 
     stopServer(): void {
         console.log("Stopped DB server");
+        this.isActive = false;
+    }
+
+    async getData() {
+        await this.syncDataFromServer();
+        console.log(`Data from db server: ${this.data}`);
+    }
+
+    async syncDataFromServer() {
+        console.log("Syncing data from DB server");
+
+        try {
+            this.data = await this.getDataAsync();
+        } catch (e) {
+            this.error = String(e);
+            console.log(`Error from db server: ${this.error}`);
+        }
+    }
+
+    private getDataAsync(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                if (this.isActive) {
+                    resolve("db at " + new Date().getTime());
+                } else {
+                    reject("500 server error");
+                }
+            }, 1000);
+        });
     }
 }
 
 const dbServer: IServer = new DbServer("127.0.0.1", "80");
 dbServer.startServer();
+dbServer.getData();
 dbServer.stopServer();
