f70ccbdf325750f377997a808fa5528afb5dc2f8
Luke Chen <lchen.dev@gmail.com>
2022-06-22 20:52:09 -0400

    cdk init

---
+++ workspace_aws/test_cdk2/test/test_cdk2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestCdk2 from '../lib/test_cdk2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test_cdk2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestCdk2.TestCdk2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
