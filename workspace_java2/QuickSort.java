abc49873e15f453e3ddb5447ee399a29b55c2479
Luke Chen <lchen.dev@gmail.com>
2024-10-04 23:46:46 -0400

    Update QuickSort.java

--- workspace_java2/QuickSort.java
+++ workspace_java2/QuickSort.java
@@ -1,1 +1,43 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
 
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+
+        int pivot = nums[(lo + (hi - lo) / 2)];
+        int i = lo;
+        int j = hi;
+        while (i <= j) {
+            while (i <= j && nums[i] < pivot) {
+                i++;
+            }
+
+            while (i <= j && nums[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                if (i < j) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+
+                i++;
+                j--;
+            }
+        }
+
+        quickSort(nums, lo, j);
+        quickSort(nums, i, hi);
+    }
+}
